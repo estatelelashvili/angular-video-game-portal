601c57ebbcd2744bd1decdf71f10957fcc9031fe
Estate Lelashvili <estatelelashvili@gmail.com>
2022-11-16 16:11:53 +0400

    feat: add functionality to game-tabs component

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Game } from 'src/app/models';
 
 @Component({
   selector: 'app-game-tabs',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-tabs.component.scss']
 })
 export class GameTabsComponent implements OnInit {
-
+@Input() game: Game = {
+  id: 0,
+  background_image: '',
+  name: '',
+  released: '',
+  metacritic_url: '',
+  website: '',
+  description: '',
+  metacritic: 0,
+  genres: [],
+  parent_platforms: [],
+  publishers: [],
+  ratings: [],
+  screenshots: [],
+  trailers: [],
+};
   constructor() { }
 
   ngOnInit(): void {
